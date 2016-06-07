6340d2694f91678a7e0c7166a91a7512fdbb2898
BidziS <danielcudnik93@gmail.com>
2016-06-08 00:56:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bidzis/aaaaaaaaa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bidzis.aaaaaaaaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
